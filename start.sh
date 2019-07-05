6a6f9cb0209f4417e527bd4b40ea403f5fe4c4fc
b.gajda <b.gajda@webard.ne>
2019-07-05 17:01:02 +0200

    dd

--- start.sh
+++ start.sh
@@ -1,16 +1,24 @@
+#!/bin/bash
 apt update
 apt dist-upgrade -yqq
-mkdir ~/.ssh
-chmod 0700 ~/.ssh
-cat <<EOF >> ~/.ssh/id_rsa.pub
-enter DD
+mkdir /home/admin/.ssh
+chmod 0700 /home/admin/.ssh
+cat <<EOF >> /root/.ssh/authorized_keys
+ADDCESS PUB HERE
 EOF
-cat <<EOF >> ~/.ssh/id_rsa
-ENTER DD
+chmod 0600 /home/admin/.ssh/authorized_keys
+ 
+mkdir /root/.ssh
+chmod 0700 /root/.ssh
+cat <<EOF >> /root/.ssh/id_rsa.pub
+PUB CERT HERE
 EOF
-chmod 0600 -R ~/.ssh
+cat <<EOF >> /root/.ssh/id_rsa
+PRIV CERT HERE
+EOF
+chmod 0600 -R /root/.ssh
 apt install git -yqq
-if [ ! -n "$(grep "^github.com " ~/.ssh/known_hosts)" ]; then ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null; fi
+if [ ! -n "$(grep "^github.com " /root/.ssh/known_hosts)" ]; then ssh-keyscan github.com >> /root/.ssh/known_hosts 2>/dev/null; fi
 git clone git@github.com:webard/sylaunch.git /root/sylaunch
 chmod 700 -R /root/sylaunch
 ln -s /root/sylaunch/launch.sh /usr/bin/sylaunch 
