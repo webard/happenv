f033c9a3000ae1b052b3ed44d3eb1a17be835ee5
b.gajda <b.gajda@webard.ne>
2019-07-06 10:31:54 +0200

    service and update improvements

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
 cd /usr/lib/happenv
 git pull
+chmod 700 -R /usr/lib/happenv
+rm /usr/bin/happenv
+ln -s /usr/lib/happenv/action.sh /usr/bin/happenv
+rm /etc/systemd/system/happenv.service
+ln -s /usr/lib/happenv/happenv.service /etc/systemd/system/happenv.service
+service happenv reenable
 service happenv restart
